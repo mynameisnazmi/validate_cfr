45c5f0c004f8454fb8f84ff0e97ee4142ee6ae0a
irfannazmi <mynameisnazmi41@gmail.com>
2023-06-18 15:16:13 +0700

    yes

--- models/UserModel.js
+++ models/UserModel.js
@@ -4,8 +4,8 @@ const { Request, TYPES } = require("tedious");
 const getUserdata = async (uid, pass) => {
   //  console.log(uid, pass);
   const allData = {};
-  const selectQuery = `SELECT userid, nama FROM username WHERE userid = @uid AND password = @pass`;
   let row = 0;
+  const selectQuery = `SELECT userid, nama FROM username WHERE userid = @uid AND password = @pass`;
 
   //Set the promise awaiting it gets results
   await new Promise((resolve, reject) => {
@@ -37,8 +37,34 @@ const getUserdata = async (uid, pass) => {
   return allData;
 };
 
-const addUserdata = async(uid, pass)=>{
-  
+const addUserdata = async(uid,nama, pass)=>{
+  //const allData = {};
+  //let row = 0;
+  const sp_name = '[dbo].[sample]'
+
+//Set the promise awaiting it gets results
+await new Promise((resolve, reject) => {
+  const request = new Request(sp_name, function (err /*, rowCount*/) {
+    if (err) {
+      return reject('Error executing stored procedure:',err);
+    } else {
+      console.log('Stored procedure executed successfully');;
+    }
+     // Close the connection after executing the stored procedure
+     connection.close();
+  });
+
+
+  request.addParameter('inputVal', TYPES.VarChar, 'hello world');
+  request.addOutputParameter('outputCount', TYPES.Int);
+
+  request.on('returnValue', (paramName, value, metadata) => {
+    //console.log(paramName + ' : ' + value);
+    return resolve(value); //Resolve allData using promise in order to get it´s content later
+  });
+  connection.callProcedure(request);
+
+});
 }
 
 
